33d215e405837e8d0557072dc3296d0937685a27
Andrey Zolotukhin <andrey.zolotukhin2017@gmail.com>
2024-10-01 02:44:00 +0300

    feat: add namespace Constants.

---
+++ lab4/include/Constants.h
@@ -0,0 +1,20 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Constants
+{
+	const int power_swordsman = 300; 
+	const int power_mage = 150;
+	const int power_shooter = 75;
+
+	const int durability_swordsman = 200;
+	const int durability_mage = 50;
+	const int durability_shooter = 100;
+
+	const int distance[] = {1, 2, 3};
+
+	const int cost_1 = 100;
+	const int cost_2 = 200;
+}
+
+#endif
