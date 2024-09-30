ac366050951d7e3b5866770a0fce212519764616
Andrey Zolotukhin <andrey.zolotukhin2017@gmail.com>
2024-10-01 00:35:43 +0300

    fix: add constructor, destructor, and pair of methods.

--- lab4/include/Guild.h
+++ lab4/include/Guild.h
@@ -1,13 +1,47 @@
+#include "Swordsman.h"
+#include "Mage.h"
+#include "Shooter.h"
+#include "Mercenary.h"
+
 class Guild
 {
 	private:
-		int power;
-		int durability;
-		int distance;
-		int cost;
-	
+		Mercenary **mercenaries;	
+		int size;
+		int capacity;
 	public:
-		void show_list()
+		Guild(int c) : size(0), capacity(c)
+			{ mercenaries = new Mercenary*[capacity]; }
+
+		~Guild()
 		{
+			for (int i = 0; i < size; i++)
+				delete mercenaries[i];
+
+			delete [] mercenaries;
+		}
+
+		void set_mercenary(void *character, const char *type)
+		{
+			if (size >= capacity)
+			{
+				capacity *= 2;
+
+				Mercenary **new_mercenaries = new Mercenary*[capacity];
+				for (int i = 0; i < size; i++)
+					new_mercenaries[i] = mercenaries[i];
+				
+				delete [] mercenaries;
+				mercenaries = new_mercenaries;
+			}
+
+			mercenaries[size++] = new Mercenary(character, type);
+		}
+
+		void get_mercenary_list()
+		{
+			std::cout << "Список наемников:" << std::endl;
+			for (int i = 0; i < size; i++)
+				std::cout << "Тип: " << mercenaries[i]->type << std::endl;
 		}
 };
