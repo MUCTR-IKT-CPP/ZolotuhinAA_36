37e511b94e28a27e5b863af23a4b43be95f523fb
Andrey Zolotukhin <andrey.zolotukhin2017@gmail.com>
2024-10-01 02:46:19 +0300

    feat: add one method.

--- lab4/include/Mercenary.h
+++ lab4/include/Mercenary.h
@@ -1,8 +1,30 @@
+#include "Swordsman.h"
+#include "Mage.h"
+#include "Shooter.h"
+
 class Mercenary
 {
 	public:	
 		void *character;
-		const char* type;
+		int type;	
 		
-		Mercenary(void *c, const char *t) : character(c), type(t) {}	
+		Mercenary(void *c, int t) : character(c), type(t) {}	
+
+		void get_mercenary_characteristic()
+		{
+			switch(type)
+			{
+				case(0):
+					static_cast<Swordsman*>(character)->Swordsman::get_charaсteristic();
+					break;
+
+				case(1):
+					static_cast<Mage*>(character)->Mage::get_charaсteristic();
+					break;
+
+				case(2):
+					static_cast<Shooter*>(character)->Shooter::get_charaсteristic();
+					break;
+			}
+		}
 };
